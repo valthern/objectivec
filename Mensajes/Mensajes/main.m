71e1200cc151dc2b66bfe73708a091a3736bfa35
Omar I. Martínez Gómez <omar_238@hotmail.com>
2021-12-21 21:36:18 -0600

    empezando proyecto de mensajes

---
+++ Mensajes/Mensajes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mensajes
+//
+//  Created by Omar I. Martínez Gómez on 21/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
